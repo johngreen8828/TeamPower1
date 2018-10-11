83e499c6285c4187472c537a92a11f11d3e320a5
John Green <johngreen8828@yahoo.com>
2018-10-11 10:18:46 -0500

    First Commit

---
+++ TeamPower1/TeamPower1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamPower1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
